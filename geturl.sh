6a85b1dfeb19837e3d5ba41c114a572c0ba48a7b
Proxx <marco@proxx.nl>
2018-10-26 13:56:18 +0200

    removed geturl.sh

--- geturl.sh
+++
@@ -1,5 +0,0 @@
-URL=$(wget -O - https://grafana.com/grafana/download?platform=arm | grep -o '<a href=['"'"'"][^"'"'"']*['"'"'"]' | sed -e 's/^<a href=["'"'"']//' -e 's/["'"'"']$//' | grep armv7.tar.gz)
-echo "wget -O /tmp/grafana.tar.gz $($URL)"
-
-
-
